e2709bf1fae260696b3fec9a5bf522205dedcac2
ibnvladimir <iidesign@yandex.ru>
2019-12-27 18:04:49 +0400

    hello

--- Sorting/src/first/Begin.java
+++ Sorting/src/first/Begin.java
@@ -1,4 +1,4 @@
-<<<<<<< HEAD
+
 package first;
 
 public class Begin {
@@ -21,27 +21,3 @@ public class Begin {
 	}
 
 }
-=======
-package first;
-
-public class Begin {
-
-	public void run() {
-		SelectionWindow beginWindow = new SelectionWindow();
-		beginWindow.show();
-
-		ArrayConstruct arr = new ArrayConstruct();
-		arr.whichWay(beginWindow);
-		arr.createDuplicate();
-		arr.sorting();
-		ResultWindow result = new ResultWindow();
-		result.show(arr);
-	}
-
-	public static void main(String[] args) {
-		Begin courseWork = new Begin();
-		courseWork.run();
-	}
-
-}
->>>>>>> branch 'master' of https://github.com/ibnvladimir/courseWork2.git
